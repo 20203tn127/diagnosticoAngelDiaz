6d6803884fbcf9cc84c852f3d97d89753672b363
AngelinhoDev <20203tn127@utez.edu.mx>
2023-09-05 17:31:05 -0600

    examenDiagnostico

---
+++ index.js
@@ -0,0 +1,42 @@
+fetch('https://reqres.in/api/users?page=2')
+.then((response)=>{
+    response.json().then((payload)=>{
+        console.log(payload.data)
+            let lista = document.getElementById("tablita");
+            payload.data.forEach(element=>{
+                lista.innerHTML += 
+                `<th scope="row">${element.id}</th>
+                <td>${element.first_name}</td>
+                <td>${element.email}</td>
+                <td>${element.last_name}</td>
+                <td ><img src="${element.avatar}" alt="" width=""></td>`;
+            })     
+    })
+})
+.catch((error)=>{
+    console.log("Error al consultar", error)
+})
+
+
+function registrarUsuario(){
+    fetch('https://reqres.in/api/users',{
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        body: {
+            "first_name": document.getElementById('nombre1'),
+            "last_name": document.getElementById('nombre2')
+        }
+    }).then((response)=>{console.log(response)})
+
+}
+
+
+
+
+
+
+
+
+
+
+

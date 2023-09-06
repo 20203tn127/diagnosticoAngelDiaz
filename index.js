b45a8ee69df2bda8a0e1ea3f5537b669bb572bb9
AngelinhoDev <20203tn127@utez.edu.mx>
2023-09-05 19:30:57 -0600

    changes

--- index.js
+++ index.js
@@ -9,13 +9,43 @@ fetch('https://reqres.in/api/users?page=2')
                 <td>${element.first_name}</td>
                 <td>${element.email}</td>
                 <td>${element.last_name}</td>
-                <td ><img src="${element.avatar}" alt="" width=""></td>`;
-            })     
+                <td ><img src="${element.avatar}" alt="" width=""></td>
+                <td><button type="button" onclick="verUsuario(${element.id})"
+                class="btn btn-primary btn-info" data-bs-toggle="modal" data-bs-target="#modalVer">
+                Ver
+              </button>
+                <button type="submit" class="btn btn-light">actualizar</button>
+                <button type="submit" class="btn btn-info">borrar</button>
+                </td>
+                `;
+            }) 
     })
 })
 .catch((error)=>{
     console.log("Error al consultar", error)
 })
+
+
+
+function verUsuario(id) {
+    let datosIndividules = document.getElementById("cardsita");
+    datosIndividules.innerHTML = ''; // Clear existing content
+
+    fetch(`https://reqres.in/api/users/${id}`)
+        .then((response) => {
+            response.json().then((payload) => {
+                console.log(payload.data);
+
+                datosIndividules.innerHTML += `
+                    <img src="${payload.data.avatar}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${payload.data.first_name}</h5>
+                        <p class="">${payload.data.email}</p>
+                    </div>`;
+            });
+        });
+}
+
 
 
 function registrarUsuario(){
